ee1c93ebfc4d184e657ebf64f6aaa78a80050eb6
Lee <lee.o.noble@gmail.com>
2018-06-28 14:14:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array){
   for (var i = 1; i < 25; i++) {
-    array[i] = ''"I am ${i} strange loop${i === 0 ? '' : 's'}."''
-  }
+    array[i] = "I am ${i} strange loop${i === 0 ? '' : 's'}."
+  } 
 }
