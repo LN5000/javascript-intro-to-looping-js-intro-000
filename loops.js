cfb9f12a2501f79d6fbddbf347ae46598a8b31fa
Lee <lee.o.noble@gmail.com>
2018-06-28 15:07:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 function forLoop() {
   array = []
   for (var i = 0; i <= 24; i++){
-    array.push(`I am ${i+1} strange loop${i === 0 ? '' : 's'}.`)
+    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
   }
   return array
 }
